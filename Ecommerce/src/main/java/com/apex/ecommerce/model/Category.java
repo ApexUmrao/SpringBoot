317f03ec4391e4137d4cedc63317d54fe6b69b25
ApexUmrao <umraoshivanshu@gmail.com>
2026-02-24 01:25:20 +0530

    Lombok Added

--- Ecommerce/src/main/java/com/apex/ecommerce/model/Category.java
+++ Ecommerce/src/main/java/com/apex/ecommerce/model/Category.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 	
 	@Id
@@ -13,28 +17,4 @@ public class Category {
     private int categoryId;
 	
     private String categoryName;
-
-    public Category() {
-	}
-
-	public Category(int categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
